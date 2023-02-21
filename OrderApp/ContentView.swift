a284e4c0c81ebb3db5d3eb75c95b6bb76f2ff662
YimKeul <leesjun29@gmail.com>
2023-02-22 00:22:39 +0900

    feat : NavigationStack and Custom Style

--- OrderApp/ContentView.swift
+++ OrderApp/ContentView.swift
@@ -9,11 +9,48 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("commit")
+        NavigationStack{
+            List{
+                VStack(alignment: .leading){
+                    Text("Salad Sandwich")
+                        .font(.largeTitle)
+                        .bold()
+                    Image(systemName: "wonsign")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 300, height: 200, alignment: .center)
+                        .background(.yellow)
+                        .cornerRadius(15)
+                    HStack{
+                        Label("4000",systemImage: "wonsign")
+                            .labelStyle(.titleAndIcon)
+                        Spacer()
+                        Label("10", systemImage: "clock" )
+                            .labelStyle(.iconAndTitle)
+                    }
+                    .font(.caption)
+                }
+                .padding()
+                .background(.blue)
+                .cornerRadius(14)
+                .navigationTitle("MainWay")
+
+            }
         }
-        .padding()
     }
+}
+
+struct IconAndTitleLabelStyle : LabelStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+extension LabelStyle where Self == IconAndTitleLabelStyle {
+    static var iconAndTitle: Self { Self() }
 }
 
 struct ContentView_Previews: PreviewProvider {
