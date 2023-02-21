f7df6514bcf26d2708888e05a73658efd086ce11
YimKeul <leesjun29@gmail.com>
2023-02-21 13:25:35 +0900

    Initial Commit

---
+++ OrderApp/OrderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrderAppApp.swift
+//  OrderApp
+//
+//  Created by yimkeul on 2023/02/21.
+//
+
+import SwiftUI
+
+@main
+struct OrderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
